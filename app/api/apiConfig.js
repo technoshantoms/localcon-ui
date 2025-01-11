f05f0f8378bcccf4ca6a6f224800f62819ee11ee
Dennis K Satia <118351475+technoshantoms@users.noreply.github.com>
2025-01-11 16:01:24 +0300

    Revert "style."
    This reverts commit 2a062464696c38d82ca5c35be8e444331717575f.

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -1,99 +1,12 @@
-import {getFaucet, getTestFaucet} from "../branding";
-export const ioxbankAPIs = {
-    BASE: "https://api.ioxbank.com/bitshares",
-    COINS_LIST: "/coins",
-    ACTIVE_WALLETS: "/active-wallets",
-    TRADING_PAIRS: "/trading-pairs",
-    NEW_DEPOSIT_ADDRESS: "/simple-api/initiate-trade"
-};
-
-export const blockTradesAPIs = {
-    BASE: "https://api.blocktrades.us/v2",
-    COINS_LIST: "/coins",
-    ACTIVE_WALLETS: "/active-wallets",
-    TRADING_PAIRS: "/trading-pairs",
-    DEPOSIT_LIMIT: "/deposit-limits",
-    ESTIMATE_OUTPUT: "/estimate-output-amount",
-    ESTIMATE_INPUT: "/estimate-input-amount"
-};
-
-export const openledgerAPIs = {
-    BASE: "https://ol-api1.openledger.info/api/v0/ol/support",
-    COINS_LIST: "/coins",
-    ACTIVE_WALLETS: "/active-wallets",
-    TRADING_PAIRS: "/trading-pairs",
-    DEPOSIT_LIMIT: "/deposit-limits",
-    ESTIMATE_OUTPUT: "/estimate-output-amount",
-    ESTIMATE_INPUT: "/estimate-input-amount",
-    RPC_URL: "https://openledger.info/api/"
-};
-
-export const rudexAPIs = {
-    BASE: "https://gateway.rudex.org/api/rudex",
-    COINS_LIST: "/coins",
-    NEW_DEPOSIT_ADDRESS: "/simple-api/initiate-trade"
-};
-
-export const bitsparkAPIs = {
-    BASE: "https://dex-api.bitspark.io/api/v1",
-    COINS_LIST: "/coins",
-    ACTIVE_WALLETS: "/active-wallets",
-    TRADING_PAIRS: "/trading-pairs",
-    DEPOSIT_LIMIT: "/deposit-limits",
-    ESTIMATE_OUTPUT: "/estimate-output-amount",
-    ESTIMATE_INPUT: "/estimate-input-amount"
-};
-
-export const cryptoBridgeAPIs = {
-    BASE: "https://api.crypto-bridge.org/api/v1",
-    COINS_LIST: "/coins",
-    ACTIVE_WALLETS: "/wallets",
-    MARKETS: "/markets",
-    TRADING_PAIRS: "/trading-pairs"
-};
-
-export const citadelAPIs = {
-    BASE: "https://citadel.li/trade",
-    COINS_LIST: "/coins",
-    ACTIVE_WALLETS: "/active-wallets",
-    TRADING_PAIRS: "/trading-pairs",
-    DEPOSIT_LIMIT: "/deposit-limits",
-    ESTIMATE_OUTPUT: "/estimate-output-amount",
-    ESTIMATE_INPUT: "/estimate-input-amount"
-};
-
-export const gdex2APIs = {
-    BASE: "https://gateway.rudex.org/api/adjust",
-    COINS_LIST: "/coins",
-    ACTIVE_WALLETS: "/active-wallets",
-    TRADING_PAIRS: "/trading-pairs"
-};
-
-// Legacy Deposit/Withdraw
-export const gdexAPIs = {
-    BASE: "https://https://gateway.rudex.org",
-    ASSET_LIST: "/gateway/asset/assetList",
-    ASSET_DETAIL: "/gateway/asset/assetDetail",
-    GET_DEPOSIT_ADDRESS: "/gateway/address/getAddress",
-    CHECK_WITHDRAY_ADDRESS: "/gateway/address/checkAddress",
-    DEPOSIT_RECORD_LIST: "/gateway/deposit/recordList",
-    DEPOSIT_RECORD_DETAIL: "/gateway/deposit/recordDetail",
-    WITHDRAW_RECORD_LIST: "/gateway/withdraw/recordList",
-    WITHDRAW_RECORD_DETAIL: "/gateway/withdraw/recordDetail",
-    GET_USER_INFO: "/gateway/user/getUserInfo",
-    USER_AGREEMENT: "/gateway/user/isAgree",
-    WITHDRAW_RULE: "/gateway/withdraw/rule"
-};
-
 export const settingsAPIs = {
-    DEFAULT_WS_NODE: "wss://wss.acloudbank.com",
+    DEFAULT_WS_NODE: "wss://ru.localcoin.is",
     WS_NODE_LIST: [
-        {url: "wss://wss.acloudbank.com", location: "connected"},
-        {url: "wss://wss.acloudbank.com", location: "US Denver by crazy-indain"},
-        {url: "wss://wss.acloudbank.com", location: "Africa"},
-        {url: "wss://wss.acloudbank.com", location: "Russia"}
+        {url: "ws://127.0.0.1:8090", location: "Locally hosted"},
+        {url: "wss://chippawa-for-life.com", location: "US Denver by crazy-indain"},
+        {url: "wss://node.gate.is", location: "Africa"},
+        {url: "wss://ru.localcoin.is", location: "Russia"}
     ],
-    DEFAULT_FAUCET: "https://faucet.acloudbank.com", // 2018-12-infrastructure worker proposal
-    TESTNET_FAUCET: "https://faucet.testnet.acloudbank.com",
+    DEFAULT_FAUCET: "https://faucet.localcoin.is", // 2018-12-infrastructure worker proposal
+    TESTNET_FAUCET: "https://faucet.testnet.localcoin.is",
     RPC_URL: "https://api.llc.is/apidocs/"
 };
